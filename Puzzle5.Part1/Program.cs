445d51346d3efdbc7e46e7474f9a8e3955202352
Liam Appleyard <liam.appleyard@drdoctor.co.uk>
2021-12-05 22:34:11 +0000

    Puzzle 5 Parts 1 & 2

---
+++ Puzzle5.Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Puzzle5.Part1;
+
+var puzzle = new Puzzle();
+var result = puzzle.Calculate();
+
+Console.WriteLine($"Overlapping line count: {result}");
+Console.ReadLine();
+//5819
+//5613
+//6461
---
+++ Puzzle5.Part2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Puzzle5.Part2;
+
+var puzzle = new Puzzle();
+var result = puzzle.Calculate();
+
+Console.WriteLine($"Overlapping line count: {result}");
+Console.ReadLine();
