6f715a5402caa56720a84174eefcb80babc98ac6
Liam Appleyard <liam.appleyard@drdoctor.co.uk>
2021-12-05 10:51:58 +0000

    Day 4 Part 1

--- Puzzle4.Part1/Program.cs
+++ Puzzle4.Part1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Puzzle4.Part1;
+
+var puzzle = new Puzzle();
+var result = puzzle.Calculate();
+
+Console.WriteLine($"Result: {result}");
